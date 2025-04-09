858fbe0868ef28cb1dbb85065fe86e76f3753880
ehtjd4893 <nsop4893@gmail.com>
2025-04-09 15:28:15 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,4 @@
+잘 못 하기 때문에 시작한 토이프로젝트입니다.
+노션에 공부하는 내용을 기록하며 작업할 예정입니다.
+
+https://www.notion.so/1b9cdd0e57b180adb8afccc0c2a1377d?v=1b9cdd0e57b1804d905f000c93e2a329&pvs=4
---
+++ src/main/java/com/why/that/ThisApplication.java
@@ -0,0 +1,13 @@
+package com.why.that;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisApplication.class, args);
+	}
+
+}
